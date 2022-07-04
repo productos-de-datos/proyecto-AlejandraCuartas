ffb288e2e951c750105121565a5d7938a40d7089
Derlis Alejandra Cuartas <dcuartasa@unal.edu.co>
2022-07-03 23:57:35 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -1,31 +1,35 @@
 """
 Módulo de ingestión de datos desde repositorio externo
-Debe ser ejecutado ya sea desde el directorio actual o desde la raiz del proyecto
 """
 import os
 import urllib.request
 
 def ingest_data():
-    """Ingeste los datos externos a la capa landing del data lake.
+    """
+    Módulo de ingestión de datos.
+    -------------------------------------------------------------------------------
+    Ingeste los datos externos a la capa landing del data lake.
+
     Del repositorio jdvelasq/datalabs/precio_bolsa_nacional/xls/ descarge los
     archivos de precios de bolsa nacional en formato xls a la capa landing. La
     descarga debe realizarse usando únicamente funciones de Python.
-    """
 
+    """
     parent_dir = "data_lake"
 
     if os.path.isdir(parent_dir) and os.path.isdir(parent_dir+"/landing" ):
         os.chdir(parent_dir + "/landing")
         for year in range(1995,2022):
-            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/'
+        url=
+        'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/'
             if year not in (2016, 2017):
-                url = url + str(year)+'.xlsx?raw=true'
+                url= url + str(year)+'.xlsx?raw=true'
                 urllib.request.urlretrieve(url, str(year) + ".xlsx")
             else :
                 url = url + str(year)+'.xls?raw=true'
                 urllib.request.urlretrieve(url, str(year) + ".xls")
     else:
-        print("There is not landing directory!")
+        print("There is not landing directory")
 
 
 if __name__ == "__main__":
